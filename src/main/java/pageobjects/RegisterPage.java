1200ce2d8415a591f9ace19a982a8b679a7b8ba8
Elena Rosca <elenroska@gmail.com>
2022-04-15 22:34:35 +0300

    Add new code to the project and define the WebDriverManager, Optimized memory usage.

---
+++ src/main/java/pageobjects/RegisterPage.java
@@ -0,0 +1,41 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPage extends Page {
+    public RegisterPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//*[@id=\"input-firstname\"]")
+    private WebElement firstNameInput;
+    @FindBy(xpath = "//*[@id=\"input-lastname\"]")
+    private WebElement lastNameInput;
+    @FindBy(xpath = "//*[@id=\"input-email\"]")
+    private WebElement emailInput;
+    @FindBy(xpath = "//*[@id=\"input-telephone\"]")
+    private WebElement phoneInput;
+    @FindBy(xpath = "//*[@id=\"input-password\"]")
+    private WebElement passwordInput;
+    @FindBy(xpath = "//*[@id=\"input-confirm\"]")
+    private WebElement confirmPassworgInput;
+    @FindBy(xpath = "//*[@id=\"content\"]/form/div/div/input[1]")
+    private WebElement privacyCheckBox;
+    @FindBy(xpath = "//*[@id=\"content\"]/form/div/div/input[2]")
+    private WebElement continueButton;
+
+    public void filliTheRegistrationPageForm(String firstName, String lastName, String email, String phone, String password, String confirmPassworg) {
+        firstNameInput.sendKeys(firstName);
+        lastNameInput.sendKeys(lastName);
+        emailInput.sendKeys(email);
+        phoneInput.sendKeys(phone);
+        passwordInput.sendKeys(password);
+        confirmPassworgInput.sendKeys(confirmPassworg);
+
+
+    }
+
+}
+
