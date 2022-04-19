6e05fd03b095577a66149931ac91f0795405a1af
Elena Rosca <elenroska@gmail.com>
2022-04-19 16:52:54 +0300

    Add new code to the project and define the JUNET, Optimized memory usage.

---
+++ src/main/java/JunitPractice.java
@@ -0,0 +1,55 @@
+import managers.WebDriverManager;
+import org.junit.jupiter.api.*;
+import pageobjects.HomePage;
+import pageobjects.RegisterPage;
+
+import static java.lang.Thread.sleep;
+
+public class JunitPractice {
+
+    WebDriverManager webDriverManagerObject = new WebDriverManager("Chrome");
+
+    @BeforeEach
+    public void beforeTestProcedure() {
+        webDriverManagerObject.getDriverRun().get("https://demo.opencart.com/\n");
+    }
+
+    @AfterEach
+    public void afterTestInstructions() {
+        webDriverManagerObject.getDriverRun().close();
+        webDriverManagerObject.getDriverRun().quit();
+    }
+
+
+    @Test
+    @DisplayName("The URL addres is displayed from Home")
+    public void verifyTheRegistrationPageIAccessible() throws InterruptedException {
+
+
+        HomePage homePage = new HomePage(webDriverManagerObject.getDriverRun());
+        homePage.navigateToRegisterPage();
+
+        RegisterPage registerPage = new RegisterPage(webDriverManagerObject.getDriverRun());
+        registerPage.filliTheRegistrationPageForm("Margarita", "Popescu", "margpop@gmail.com", "0224565856", "4865555525", "4865555525");
+
+        sleep(3000);
+        boolean urlIsValide = webDriverManagerObject.getDriverRun().getCurrentUrl().contains("register");
+        Assertions.assertTrue(urlIsValide, "Address URL is not valid");
+        
+    }
+
+    @Test
+    @DisplayName("Register page elements are displayed")
+    public void registerPageElementeAreDisplayed() throws InterruptedException {
+
+
+        HomePage homePage = new HomePage(webDriverManagerObject.getDriverRun());
+        homePage.navigateToRegisterPage();
+
+        sleep(3000);
+        RegisterPage registerPage = new RegisterPage(webDriverManagerObject.getDriverRun());
+        Assertions.assertTrue(registerPage.allTheElementsAreDisplayed(), "Cel putin unul dintre elemente nu a fost afisat");
+
+    }
+
+}
